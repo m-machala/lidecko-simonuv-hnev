e1f259059630f0bd17335d29e4b2577b06f01120
simonslovak <121507132+simonslovak@users.noreply.github.com>
2025-02-12 01:17:47 +0100

    initial assets with idle

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -7,6 +7,7 @@ using UnityEngine.UIElements;
 public class Character : MonoBehaviour
 {
     public GameManager gameManager;
+    public Animator animator;
     public List<(Vector2, float)> nextPositions = new List<(Vector2, float)>();
     Vector2 previousPosition;
     public bool moving = false;
@@ -27,6 +28,7 @@ public class Character : MonoBehaviour
     }
 
     public void Move(List<Vector2> positions, float stepTime) {
+        // animator.SetFloat("Speed", 1.0f);
         foreach (Vector2 position in positions) {
             nextPositions.Add((position, stepTime));
         }
@@ -34,26 +36,52 @@ public class Character : MonoBehaviour
         moving = true;
     }
 
-    void Update() {
-        if (nextPositions.Count == 0 && moving) {
-            moving = false;
-            gameManager.FinishedMoving();
+    void Update() {       
+    animator = GetComponentInChildren<Animator>();       
+    if (nextPositions.Count == 0 && moving) {        
+        moving = false;            
+        gameManager.FinishedMoving();   
+        // animator.SetFloat("Speed", 0.0f);  
+    }
+
+    if (nextPositions.Count == 0) {
+        return;
+    }      
+         
+    float travelDistance = Vector2.Distance(previousPosition, nextPositions[0].Item1);
+    elapsedMovementTime += Time.deltaTime;    
+    if (elapsedMovementTime >= nextPositions[0].Item2 * travelDistance) {
+        elapsedMovementTime = 0;
+       
+        transform.position = new Vector3(nextPositions[0].Item1.x, transform.position.y, nextPositions[0].Item1.y);
+        
+        Vector2 newDirection = nextPositions[0].Item1 - previousPosition;
+        if (newDirection != Vector2.zero) {
+            float angle = Mathf.Atan2(newDirection.x, newDirection.y) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, angle, 0);
         }
 
-        if (nextPositions.Count == 0) {
-            return;
-        }
-        float travelDistance = Vector2.Distance(previousPosition, nextPositions[0].Item1);
-        elapsedMovementTime += Time.deltaTime;
-        if (elapsedMovementTime >= nextPositions[0].Item2 * travelDistance) {
-            elapsedMovementTime = 0;
-            transform.position = new Vector3(nextPositions[0].Item1.x, transform.position.y, nextPositions[0].Item1.y);
-            previousPosition = nextPositions[0].Item1;
-            nextPositions.RemoveAt(0);
-            return;
-        }
-        transform.position = Vector3.Lerp(new Vector3(previousPosition.x, transform.position.y, previousPosition.y), new Vector3(nextPositions[0].Item1.x, transform.position.y, nextPositions[0].Item1.y), elapsedMovementTime / (nextPositions[0].Item2 * travelDistance));    
-    }
+        previousPosition = nextPositions[0].Item1;
+        nextPositions.RemoveAt(0);
+        return;
+    }    
+
+    Vector3 newPos = Vector3.Lerp(
+        new Vector3(previousPosition.x, transform.position.y, previousPosition.y),
+        new Vector3(nextPositions[0].Item1.x, transform.position.y, nextPositions[0].Item1.y),
+        elapsedMovementTime / (nextPositions[0].Item2 * travelDistance)
+    );
+
+    transform.position = newPos;
+
+    Vector2 direction = nextPositions[0].Item1 - previousPosition;
+    if (direction != Vector2.zero) {
+        float targetAngle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+        Quaternion targetRotation = Quaternion.Euler(0, targetAngle, 0);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 10f); 
+    }    
+}
+
     void OnMouseDown()
     {
         gameManager.TileClicked(new Vector2((int)transform.position.x, (int)transform.position.z));
